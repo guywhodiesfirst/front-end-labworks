f8de7867f6eccc79707551899f5f34d3d2d49af8
guywhodiesfirst <brizeout@gmail.com>
2024-10-22 17:26:06 +0300

    Labwork 5: finish task 1

--- lab-5/script.js
+++ lab-5/script.js
@@ -7,8 +7,53 @@ const emailRegex = /^([a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3})$/
 const regularExpressions = [nameRegex, groupRegex, phoneRegex, addressRegex, emailRegex]
 const inputFields = Array.from(document.querySelectorAll(".text-input"))
 
+const submitBtn = document.getElementById("submit-btn")
+const userInfo = document.getElementById("user-info")
+
 const fieldRegexPairs = inputFields.map((item, index) => ({
     field: item,
     regex: regularExpressions[index]
 }))
+submitBtn.addEventListener("click", () => {
+    let flag = true;
+    
+    // Validation of fields
+    fieldRegexPairs.forEach((pair) => {
+        if (!pair.regex.test(pair.field.value)) {
+            flag = false;
+            pair.field.classList.add("input-fail");
+        } else {
+            pair.field.classList.remove("input-fail");
+        }
+    });
 
+    if (flag) {
+        userInfo.innerHTML = ""
+        removeClass(userInfo, 'hide')
+        const header = document.createElement('h2');
+        header.textContent = "Введені дані:";
+        userInfo.appendChild(header);
+
+        inputFields.forEach((field) => {
+            let paragraph = document.createElement("p");
+            paragraph.textContent = field.value;  
+            userInfo.appendChild(paragraph);
+        });
+    }
+});
+
+function removeClass(element, nameOfClass) {
+    if (element.classList.contains(nameOfClass)) {
+        element.classList.remove(nameOfClass)
+    }
+}
+
+for (let i = 0; i < inputFields.length; i++) {
+    inputFields[i].setAttribute('autocomplete', 'none')
+    inputFields[i].addEventListener("focus", function() {
+        removeClass(this, 'input-fail')
+        if(!userInfo.classList.contains('hide')) {
+            userInfo.classList.add('hide')
+        } 
+    })
+}
